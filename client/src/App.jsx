9317e67f6d9a04e679028eab1b4e82ba071d1c37
Indranil Halder <indranilhalder.dev@gmail.com>
2022-08-25 23:24:26 +0530

    Setup React Redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,21 +4,26 @@ import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Home from "./components/pages/Home";
 import Product from "./components/pages/Product";
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <main className="py-4">
-        <Container>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/product/:productId" element={<Product />} />
-          </Routes>
-        </Container>
-      </main>
-      <Footer />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <main className="py-4">
+          <Container>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/product/:productId" element={<Product />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </BrowserRouter>
+    </Provider>
   );
 };
 
