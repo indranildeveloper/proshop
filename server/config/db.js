55d3353ac5fb0b4ebbafa2f98808718e19151fb9
Indranil Halder <indranilhalder.dev@gmail.com>
2023-02-03 02:32:38 +0530

    Prepared for production

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ import colors from "colors";
 
 const connectDB = async () => {
   try {
-    const db = await mongoose.connect(process.env.MONGO_URI);
+    const db = await mongoose.connect(process.env.MONGO_PRODUCTION_URI);
     console.log(`MongoDB Connected: ${db.connection.host}`.cyan.underline);
   } catch (err) {
     console.error(`Error: ${err.message}`.red.bold.underline);
