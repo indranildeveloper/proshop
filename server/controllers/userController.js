4408f428473df07e8dbea70cac16eddc066dbf68
indranilcoder <125015385+indranilcoder@users.noreply.github.com>
2023-04-10 02:36:11 +0530

    Update comment again

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -140,7 +140,6 @@ const getUserById = asyncHandler(async (req, res) => {
   @desc    Update user
   @route   PUT /api/users/:userId
   @access  Private/Admin
-  
 */
 const updateUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.userId);
